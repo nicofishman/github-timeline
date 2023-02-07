21e4184a748cba1de188d69e434501009369ceed
nicofishman <nicourifishman@gmail.com>
2023-02-07 17:33:21 -0300

    run prettier on .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-    "tabWidth": 4,
-    "useTabs": false,
-    "bracketSpacing": true,
-    "semi": true,
-    "singleQuote": true,
-    "trailingComma": "all",
-    "jsxSingleQuote": true,
-    "bracketSameLine": false,
-    "plugins": [require("prettier-plugin-tailwindcss")],
-    "tailwindConfig": "./tailwind.config.js",
-    "overrides": [
+    tabWidth: 4,
+    useTabs: false,
+    bracketSpacing: true,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    jsxSingleQuote: true,
+    bracketSameLine: false,
+    plugins: [require('prettier-plugin-tailwindcss')],
+    tailwindConfig: './tailwind.config.js',
+    overrides: [
         {
-            "files": "*.svg",
-            "options": {
-                "parser": "html"
-            }
-        }
-    ]
-}
+            files: '*.svg',
+            options: {
+                parser: 'html',
+            },
+        },
+    ],
+};
