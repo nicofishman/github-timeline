1e149a43e21bb62e39cf78768825b8fdf60ef0bb
nicofishman <nicourifishman@gmail.com>
2023-02-08 20:46:01 -0300

    configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App';
+import Index from './pages/index';
+import GithubProfile from './pages/[...githubProfile]';
 import './index.css';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Index />,
+    },
+    {
+        path: '/repos/:githubProfile',
+        element: <GithubProfile />,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>,
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                bebas: ["Bebas Neue", "sans-serif"],
+            }
+        },
     },
     plugins: [],
 }
